7ed31e75b899d6e7b00ad479691f0439a74f49f8
Brandon Roberts <brandonlroberts@outlook.com>
2022-10-25 20:52:51 -0400

    Add project files.

---
+++ TimePicker.Models/AmPm.cs
@@ -0,0 +1,25 @@
+﻿namespace TimePicker.Models
+{
+    public class AmPm
+    {
+        public AmPm(string name)
+        {
+            Name = name;
+        }
+
+        public readonly string Name;
+
+        // Note: this is important so the MudSelect can compare pizzas
+        public override bool Equals(object o)
+        {
+            var other = o as AmPm;
+            return other?.Name == Name;
+        }
+
+        // Note: this is important too!
+        public override int GetHashCode() => Name?.GetHashCode() ?? 0;
+
+        // Implement this for the Pizza to display correctly in MudSelect
+        public override string ToString() => Name;
+    }
+}
